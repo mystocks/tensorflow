7991d58f14ebcf9cf7ac2a6510522044b637f269
mystocks <935857786@qq.com>
2018-05-15 23:44:33 +0800

    * add tensorflow mnist test demo src

---
+++ mnistDemo/mnistMain.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+
+
+import input_data
+import tensorflow as tf
+# 调用input_data获取数据
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# 图片数据输入
+x = tf.placeholder("float", [None, 784])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+y_ = tf.placeholder("float", [None,10])
+
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(2000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+print(sess.run(W), sess.run(b))
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print (sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
